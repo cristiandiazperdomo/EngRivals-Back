ed6205c7f74c36e5d6719a67e1d6e7618e16504c
cristiandiazperdomo <danubiodiaz2003edf@gmail.com>
2024-01-10 20:24:07 -0300

    init

---
+++ src/main/java/web/app/engrivals/engrivals/EngrivalsApplication.java
@@ -0,0 +1,13 @@
+package web.app.engrivals.engrivals;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EngrivalsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EngrivalsApplication.class, args);
+	}
+
+}
